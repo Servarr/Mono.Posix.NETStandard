8c6a324177ed44b24f5767c64cf6d7d625a6bd97
ta264 <ta264@users.noreply.github.com>
2021-11-11 21:38:37 +0000

    Fix osx stripping

--- osx-arm64/compile.sh
+++ osx-arm64/compile.sh
@@ -7,7 +7,7 @@ OUT_DIR=$1
 
 pushd $SCRIPT_DIR
 
-CC=clang CXX=clang++ MACOSX_DEPLOYMENT_TARGET=11.0 CFLAGS="-arch arm64" ./autogen.sh \
+CC=clang CXX=clang++ CFLAGS="-arch arm64 -mmacosx-version-min=10.13" ./autogen.sh \
   --build=x86_64-apple-darwin20.6.0 \
   --host=aarch64-apple-darwin20.6.0
 
@@ -18,7 +18,7 @@ make
 cd "$SCRIPT_DIR/support"
 make
 
-strip -x -c .libs/libMonoPosixHelper.dylib -o "$OUT_DIR/libMonoPosixHelper.dylib"
+cp .libs/libMonoPosixHelper.dylib "$OUT_DIR/libMonoPosixHelper.dylib"
 
 otool -l ${OUT_DIR}/libMonoPosixHelper.dylib
 otool -L ${OUT_DIR}/libMonoPosixHelper.dylib
--- osx-x64/compile.sh
+++ osx-x64/compile.sh
@@ -7,7 +7,9 @@ OUT_DIR=$1
 
 pushd $SCRIPT_DIR
 
-CC=clang CXX=clang++ MACOSX_DEPLOYMENT_TARGET=10.13 ./autogen.sh
+clang --help
+
+CC=clang CXX=clang++ CFLAGS="-mmacosx-version-min=10.13" ./autogen.sh
 
 cd "$SCRIPT_DIR/mono/eglib"
 make
@@ -15,7 +17,7 @@ make
 cd "$SCRIPT_DIR/support"
 make
 
-strip -x -c .libs/libMonoPosixHelper.dylib -o "$OUT_DIR/libMonoPosixHelper.dylib"
+cp .libs/libMonoPosixHelper.dylib "$OUT_DIR/libMonoPosixHelper.dylib"
 
 otool -l ${OUT_DIR}/libMonoPosixHelper.dylib
 otool -L ${OUT_DIR}/libMonoPosixHelper.dylib
